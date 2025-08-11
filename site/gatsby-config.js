c74985f67f9625814d8d434280a58234b4308d97
Shahbaz Mughal <shahbaz.mughal@hostingshouse.com>
2025-08-11 14:49:22 +0500

    changes fixed

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,5 +1,5 @@
 // 👇 Disable TLS error (for self-signed certs, dev only)
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 // ✅ Get API URL from env
 const STRAPI_API_URL = "https://strapi5-dev-jt.mumara.com";
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "https://strapi5-dev-jt.mumara.com/api", // <-- Check this URL carefully
+        apiURL: "https://s7.r1.hostingshouse.com/api", // <-- Check this URL carefully
         // accessToken: "YOUR_STRAPI_TOKEN", // optional
         collectionTypes: ["homehero", "clintlogo", "featureone", "highlightedfeature", "whychooseus", "featuretwo", "stat", "strategie", "download", "testimonial", "getstarted", "blog", "saashero", "saasstrategie", "saasfeaturetabone", "saasfeaturetabtwo", "pricing", "saasv2featureone", "saasv2featuretwo", "saasv2featurethree", "tab", "saasv2screenshort", "saasv2feature", "process", "faq", "fintechfeature", "fintchfeatureone", "fintchfeaturetwo", "fintchwhychooseus", "featurethreemore", "mobilehero", "mobilefeatureone", "mobilefeaturetwo", "mobilefeaturethree", "markeeetingfeature", "pricing03testmonial", "aboutusone", "aboutusoneteam", "aboutustwostrategie", "servicesonehero", "servicesonefeature", "servicesonecalltoaction", "servicestwohome", "servicestwoservice", "servicesjoincompanie", "servicestwophoto", "servicesthreeblog", "servicesthreecontent", "base", "seomain", "seometa"],
       singleTypes: ["footer", "header"],
